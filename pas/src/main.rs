84d30ba78c0eba4437365082b1aea8fb63ee4838
MikeFilimonov <filimonov.michael@gmail.com>
2022-07-21 09:27:22 +0300

    resolving future

--- pas/src/main.rs
+++ pas/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+     future::Future,
+     pin::Pin,
+     task::{Context, Poll},
+    };
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    
+    let fut = MyFuture{};
+    fut.await
+
+  }
+
+  struct MyFuture {}
+   
+  impl Future for MyFuture {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
+        Poll::Ready(())
+    }
 }
